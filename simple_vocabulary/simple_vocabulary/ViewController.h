e8dc9ae4e224d8b15a9031fe8ca572dc1009ec7c
Aleksandr Ivashchenko <al.vl.ivashchenko@gmail.com>
2016-04-20 13:14:21 +0300

    Initial commit

---
+++ simple_vocabulary/simple_vocabulary/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  simple_vocabulary
+//
+//  Created by Aleksandr Ivashchenko on 4/20/16.
+//  Copyright © 2016 Aleksandr Ivashchenko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
